79cf4f5228c55f86ddec0d1053a2c191a4cb50f2
yabwon <yabwon@gmail.com>
2014-12-07 03:57:08 +0100

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -27,16 +27,16 @@ cat(
 
 # create data.table
 data.set <- data.table(
-  read.table( file = "my.small.data" 
-              , na.strings = "?"
-              , stringsAsFactors = F
-              , header = FALSE
-              , quote = ""
-              , sep = ";"
-              , dec = "."
-              , colClasses = c("character", "character", rep("numeric",7))
-  )
-)
+            read.table( file = "my.small.data" 
+                      , na.strings = "?"
+                      , stringsAsFactors = F
+                      , header = FALSE
+                      , quote = ""
+                      , sep = ";"
+                      , dec = "."
+                      , colClasses = c("character", "character", rep("numeric",7))
+                      )
+                  )
 # drop temporary file
 unlink("my.small.data")
 
@@ -49,87 +49,33 @@ setnames(data.set, c("Date"
                      , "Sub_metering_1"
                      , "Sub_metering_2"
                      , "Sub_metering_3")
-)
+         )
 # creating new timestamp from Date and Time columns
 data.set[, c("time.stamp", "Date", "Time") := {
-  tmp.time <- as.character(paste(as.Date(Date, "%d/%m/%Y"), Time, sep = ' '));
-  tmp.time <- as.POSIXct(strptime(tmp.time, "%Y-%m-%d %H:%M:%S"));
-  list(tmp.time,NULL,NULL)
-}]
+        tmp.time <- as.character(paste(as.Date(Date, "%d/%m/%Y"), Time, sep = ' '));
+        tmp.time <- as.POSIXct(strptime(tmp.time, "%Y-%m-%d %H:%M:%S"));
+        list(tmp.time,NULL,NULL)
+        }]
 
 # look through summary
 str(data.set)
 summary(data.set)
 
-
-# set international dates and time
-lct <- Sys.getlocale("LC_TIME")
-lct
-Sys.setlocale("LC_TIME", "C")
-
 # open device
-png(filename = "plot4.png"
+png(filename = "plot1.png"
     , width = 480
     , height = 480
     , units = "px"
     , pointsize = 12
     , bg = "transparent"
 )
-# setting multiple plots
-par(mfrow = c(2, 2))
-
-# create plot for Global_active_power
-plot(data.set[, time.stamp]
-     , data.set[, Global_active_power]
-     , type = "l"
-     , main = ""
-     , xlab = ""
-     , ylab = "Global Active Power"
-)
-
-# create plot for Voltage
-plot(data.set[, time.stamp]
-     , data.set[, Voltage]
-     , type = "l"
-     , main = ""
-     , xlab = "datetime"
-     , ylab = "Voltage"
-)
-
-# create plot Sub_metering_1, Sub_metering_2 and Sub_metering_3
-plot(data.set[, time.stamp]
-     , data.set[, Sub_metering_1]
-     , col = 'black'
-     , type = "l"
-     , main = ""
-     , xlab = ""
-     , ylab = "Energy sub metering"
-)
-lines(data.set[, time.stamp]
-      , data.set[, Sub_metering_2]
-      , col = 'red'
-)
-lines(data.set[, time.stamp]
-      , data.set[, Sub_metering_3]
-      , col = 'blue'
-)
-legend("topright"
-       , lty = 1
-       , col = c("black", "red", "blue")
-       , legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
-       , bty = "n"
-       )
-
-# create plot for Global_reactive_power
-plot(data.set[, time.stamp]
-     , data.set[, Global_reactive_power]
-     , type = "l"
-     , main = ""
-     , xlab = "datetime"
-     , ylab = "Global_reactive_power"
-)
-
+# create plot
+hist(data.set[, Global_active_power]
+     , col = "red"
+     , border = "black"
+     , main = "Global Active Power"
+     , xlab = "Global Active Power (kilowatts)"
+     , ylim = range(c(0,1200))
+     )
 # close device
 dev.off()
-# get back to orycinal datetime settings
-Sys.setlocale("LC_TIME", lct)  
